6a6d9a0b59df41dffee140e7cd4314e6342e55f9
Zakaria-Fahim <zakaria.fahim@uit.ac.ma>
2024-02-22 15:36:52 +0100

    get data and set Data

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,80 @@
 <template>
-<nav class="navbar navbar-light bg-light">
-        <router-link to="/">Home</router-link>
-        <router-link to="/contact">Contact</router-link>
-        <router-link to="/about">About</router-link>
-</nav>
-    <div>
-        
-    </div>
-    <router-view></router-view>
+   <div class="container">
+    <form @submit.prevent="AddStudent"> 
+      <div class="form-row">
+        <div class="form-group col-md-6">
+          <label for="inputEmail4">Name</label>
+          <input type="text" class="form-control" v-model="Student.Name" placeholder="Zakaria Fahim" required>
+        </div>
+        <div class="form-group col-md-6">
+          <label for="inputPassword4">Age</label>
+          <input type="number" v-model="Student.Age" class="form-control" placeholder="23"  required>
+        </div>
+        <div class="form-group col-md-12">
+          <label for="inputPassword4">School</label>
+          <input type="password" v-model="Student.School" class="form-control" placeholder="Lycee Abdelmalek Saadi"  required>
+        </div>
+        <div class="mb-4">
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" v-model="Student.Gender" type="radio" name="inlineRadioOptions" id="inlineRadio1" value="Male" >
+            <label class="form-check-label" for="inlineRadio1">Male</label>
+          </div>
+          <div class="form-check form-check-inline">
+            <input class="form-check-input" type="radio" v-model="Student.Gender" name="inlineRadioOptions" id="inlineRadio2" value="Female" >
+            <label class="form-check-label" for="inlineRadio2">Female</label>
+          </div>
+        </div>
+      </div>
+      <button type="submit" class="btn btn-primary">Add</button>
+    </form>
+    <Show :Table="G_Student" />
+   </div>
+
 </template>
 
 <script>
+  import Show from './components/Show.vue';
 export default {
-    
+  components:{
+     Show
+  },
+    data(){
+      return {
+        Student:{
+          id:'',
+          Name:'',
+          Age:'',
+          Gender:'',
+          School:''
+        },
+        url:"http://localhost:3000/Student/",
+        //Get Students
+        G_Student:[]
+      }
+    },
+    methods:{
+      AddStudent(){
+        this.Student.id=String(this.G_Student.length+1),
+        fetch(this.url,{
+          method:'post',
+          header:{'Content-Type':'applicatio/json'},
+          body:JSON.stringify(this.Student)
+        })
+          .then(done => console.log(done))
+          .catch(error => console.log(error))
+      }
+    },
+    mounted(){
+       fetch(this.url)
+          .then(res => res.json())
+          .then(data =>this.G_Student = data)
+          .catch(error => console.log(error))
+    }
 }
 </script>
-<style lang="">
-    
+<style scoped>
+    .container{
+      width:65%;
+      margin-top:20px;
+    }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,17 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import Home from '../views/Home.vue'
-import About from '../views/About.vue'
-import contact from '../views/contact.vue'
+import edit from '../views/edit.vue'
+import App from "../App.vue"
 
 const routes = [
     {
         path:'/',
-        name:'Home',
-        component:Home
+        name:'App',
+        component:App
     },
     {
-        path:'/about',
-        name:'About',
-        component:About
-    },
-    {
-        path:'/contact',
-        name:'contact',
-        component:contact
+        path:'/edit',
+        name:'edit',
+        component:edit
     }
 ]
 
@@ -25,6 +19,5 @@ const router = createRouter({
     history: createWebHistory(process.env.NODE_ENV === 'production' ? '/your-base-url/' : '/'),
     routes
 })
-
 export default router
 
